45797c36fa80c6fa9400efda1812ebb0d10feaa1
asdfghjkl2005 <114497171+asdfghjkl2005@users.noreply.github.com>
2022-09-27 09:50:10 -0500

    Create tarifa1X.c

---
+++ tarifa1X.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+int tarifa1A(int consumo)
+{     
+    float consumo_basico = 0;
+    float consumo_Ibasico = 0;
+    float consumo_Ialto = 0;
+    float consumo_Excedente = 0;
+
+    if (consumo >= 0 && consumo <= 75)
+    {
+        consumo_basico = 0.786 * consumo;
+        printf("Consumo basico \n");   
+    }
+     if (consumo >= 76 && consumo <= 140)
+    {
+        consumo_basico = 75 * .786;
+        consumo_Ibasico = (consumo - 75) * 0.911;
+        printf("Consumo I.bajo \n");           
+    }
+ 
+     if (consumo >= 141)
+    {
+        consumo_basico = 75 * .786; // consumo basico
+        consumo_Ibasico = (140-75) * .911; // consumo I. BAjo
+        consumo_Excedente = (consumo - 140) * 3.134;
+
+        printf("Consumo excedente \n");    
+    }
+    printf("Cobro por consumo basico: %.2f\n",consumo_basico);
+    printf("Cobro por consumo I. bajo: %.2f\n",consumo_Ibasico);
+    printf("Cobro por consumo excedente: %.2f\n",consumo_Excedente);
+    return (consumo_Excedente + consumo_Ibasico + consumo_basico);
+
+}
