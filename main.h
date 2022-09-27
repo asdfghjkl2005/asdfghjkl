6c0c31e0db359d1cbc3e54d74aed25ad21dbcf09
asdfghjkl2005 <114497171+asdfghjkl2005@users.noreply.github.com>
2022-09-27 09:03:19 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int tarifa1A(int consumo);
+int tarifa1B(int consumo);
+int tarifa1C(int consumo);
+int tarifa1D(int consumo);
+int tarifa1E(int consumo);
+int tarifaF(int consumo);
+int tarifa1Xc(int consumo);
+
+
+#endif
